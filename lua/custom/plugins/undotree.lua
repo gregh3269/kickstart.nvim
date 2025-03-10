e38a533b78eff840330e19b9757c7797305ce054
gregh <gregh3269gmail.com>
2025-03-10 19:36:20 +0000

    Swap undotree

--- lua/custom/plugins/undotree.lua
+++ lua/custom/plugins/undotree.lua
@@ -1,10 +1,15 @@
 return {
-  'jiaoshijie/undotree',
-  dependencies = 'nvim-lua/plenary.nvim',
-  config = true,
-  keys = { -- load the plugin only when using it's keybinding:
-    { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>", desc = 'Undotree' },
-  },
+  'mbbill/undotree',
+  config = function()
+    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Undotree' })
+  end,
+
+  --  'jiaoshijie/undotree',
+  --  dependencies = 'nvim-lua/plenary.nvim',
+  --  config = true,
+  --  keys = { -- load the plugin only when using it's keybinding:
+  --    { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>", desc = 'Undotree' },
+  --  },
 
   --[[ 
   config = function()
