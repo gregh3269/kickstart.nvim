d2ab5c4d491bd7efd2643aa13de5f234aedc7fe6
gregh <gregh3269@gmail.com>
2026-01-20 11:11:39 +0000

    Updates as kickstart is eol

--- init.lua
+++ init.lua
@@ -716,6 +716,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'tree-sitter-cli', -- Greg for tree sitter
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -945,7 +946,7 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter.config', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,6 +7,23 @@
 -- Save undo history. No undo from previous edit, was true.
 vim.o.undofile = false
 
+-- always set autoindenting on
+vim.o.autoindent = true
+-- use smart indent if there is no indent file
+vim.o.smartindent = true
+-- <tab> inserts 4 spaces
+vim.o.tabstop = 4
+-- <BS> over an autoindent deletes 4 spaces.
+vim.o.softtabstop = 4
+-- Handle tabs more intelligently"
+vim.o.smarttab = true
+-- Use spaces, not tabs, for autoindent/tab key.
+vim.o.expandtab = true
+-- an indent level is 4 spaces wide.
+vim.o.shiftwidth = 4
+--rounds indent to a multiple of shiftwidth
+vim.o.shiftround = true
+
 -- Load
 require 'custom.config.autocmds'
 require 'custom.config.keymaps'
