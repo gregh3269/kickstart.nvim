17e48eaae55ffb58b955d479e037de5e1305fa6c
gregh <gregh3269@gmail.com>
2026-02-07 10:43:38 +0000

    Toggle on open

--- lua/custom/plugins/undotree.lua
+++ lua/custom/plugins/undotree.lua
@@ -3,6 +3,7 @@ return {
   lazy = false,
   config = function()
     vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Undotree' })
+    vim.g.undotree_SetFocusWhenToggle = 1
   end,
 
   --  'jiaoshijie/undotree',
